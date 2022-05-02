66ef18c4e35be9009e907317f10df6e1a7ed9bd6
Angel <angelSistemas21@gmail.com>
2022-05-02 03:25:01 -0500

    test: Test de prueba User - Error

---
+++ test/models/User.test.js
@@ -0,0 +1,17 @@
+const User = require ('./../../app/models/User')
+
+describe('Unit Tests for User class', () => {
+    test('Create an User object', () => {
+        // Aquí invocas el código que vas a usar en la app
+        const user = new User(1, "AngelDiaz", "Angel", "Bio", "dateCreated", "lastUpdated")
+
+        // Aquí validas los resultados de ese código
+        // Esta es una comparación que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+        expect(user.id).toBe(1)
+        expect(user.username).toBe("AngelDiaz")
+        expect(user.name).toBe("Angel")
+        expect(user.bio).toBe("Bio")
+        expect(user.dateCreated).toBe("dateCreated")
+        expect(user.lastUpdated).toBe("lastUpdated")
+    }) ;
+})
