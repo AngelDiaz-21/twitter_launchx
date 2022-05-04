6cd6e0d46d93539076a6a4b5c67044ab12efa064
Angel <angelSistemas21@gmail.com>
2022-05-03 20:27:57 -0500

    feat: Verificación de creación de un objeto User al enviar un payload con las propiedades username, id y name

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -4,10 +4,15 @@ class UserView{
     static createUser(payload){
         if(payload === null){
             return{ error: "payload no existe"}
-        }else if((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.name !== 'string' || payload.name === null) || (typeof payload.username !== 'number' || payload.id === null)){
+        }else if((typeof payload.username !== 'string' || payload.username === null) || (typeof payload.name !== 'string' || payload.name === null) || (typeof payload.id !== 'number' || payload.id === null)){
             return {error: "necesitan tener un valor válido"}
+        }else {
+            return UserService.create(payload.id, payload.username, payload.name)
         }
     }
 }
+// const payload = { username: "username", id: 1, name: "name"}
+// const result = UserView.createUser(payload)
+// console.log(result)
 
 module.exports = UserView;
